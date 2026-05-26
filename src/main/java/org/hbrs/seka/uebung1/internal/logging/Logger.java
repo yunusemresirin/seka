864c8930a51708a402fa4a685d72d9e89f64d6fa
Yunus Emre Sirin <yunus.sirin@smail.inf.h-brs.de>
2026-05-26 17:50:57 +0200

    Removed files from Uebung-1

--- src/main/java/org/hbrs/seka/uebung1/internal/logging/Logger.java
+++
@@ -1,5 +0,0 @@
-package org.hbrs.seka.uebung1.internal.logging;
-
-public interface Logger {
-    void log(String message);
-}
--- src/main/java/org/hbrs/seka/uebung1/port/LifecycleState.java
+++
@@ -1,6 +0,0 @@
-package org.hbrs.seka.uebung1.port;
-
-public enum LifecycleState {
-    OPEN,
-    CLOSED
-}
